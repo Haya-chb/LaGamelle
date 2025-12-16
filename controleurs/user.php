86066de00ed6ea5d251e955eb9da45824ba2d5cd
Nana <nathaliavilasse@gmail.com>
2025-12-16 14:13:23 +0100

    Update user.php

--- controleurs/user.php
+++ controleurs/user.php
@@ -5,14 +5,6 @@ require_once('../modeles/favoris.php');
 $userId = 1;
 $user = getUserById($db, $userId);
 
-
-//Supprime des favoris
-if (isset($_GET['action'], $_GET['id_recette'])) {
-    //$idRecette = $_GET['id_recette'];
-    supFavoris($db, $userId, $_GET['id_recette']);
-    header('Location: ../vues/profil.php');
-}
-
-// puis affichage normal de la page
+// Aaffichage des favoris de l'utilisateur
 $favoris = getFavorisByUser($db, $userId);
 ?>
