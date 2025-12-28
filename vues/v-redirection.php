d0729cf57c9971a43715ab18290f3dde4fb4299e
Haya-chb <haya.chaibi@hotmail.com>
2025-12-28 12:08:51 +0100

    Created redirection page

---
+++ vues/v-redirection.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<header>
+    <nav>
+            <ul class="navbar">
+                <li><a href="../index.php">LG</a></li>
+                <li><a href="recette.php" class="active">Nos Recettes</a></li>
+                <li><a href="">Aliments toxiques</a></li>
+                <li><a href="">Trouver un vétérinaire</a></li>
+                <li><a href="">Proposer une recette</a></li>
+        </ul>
+        <div class="connexion">
+                <a href="">Inscription</a>
+                <a href="">Connexion</a>
+        </div>
+    </nav>
+</header>
+
+<main>
+
+<h1>Merci pour votre contribution</h1>
+
+<p>Nous allons examiner votre recette ! Si elle apprécié nous l'ajouterons à notre catalogue</p>
+
+<a href="../index.php">Retour à l'acceuil</a>
+<a href="v-contribution.php">Proposer une nouvelle recette</a>
+
+</main>
+</body>
+</html>
