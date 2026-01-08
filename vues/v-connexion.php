d383de5360b4529e079ece959e1f2c0d7f735d51
Haya-chb <haya.chaibi@hotmail.com>
2026-01-08 14:41:52 +0100

    CSS form

--- vues/v-connexion.php
+++ vues/v-connexion.php
@@ -4,31 +4,43 @@ include ("../controleurs/c-connexion.php");
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/form.css">
     <link rel="stylesheet" href="../assets/css/style.css">
-    <title>Document</title>
+
+    <title>Connexion</title>
 </head>
 <body>
     
- <header>
-        <nav>
+<header>
+        <a href="#" class="logo">LG</a>
+        <button class="burger" aria-label="Ouvrir le menu" aria-expanded="false" aria-controls="menu">
+            <img src="assets/images/burger-menu.svg" alt="">
+        </button>
+
+        <nav id="menu" aria-label="Navigation principale">
             <ul class="navbar">
-                <li><a href="#">LG</a></li>
                 <li><a href="vues/recette.php">Nos Recettes</a></li>
                 <li><a href="">Aliments toxiques</a></li>
                 <li><a href="">Trouver un vétérinaire</a></li>
                 <li><a href="">Proposer une recette</a></li>
             </ul>
+          <div class="connexion">
+                        <a href="vues/v-inscription.php">Inscription</a>
+                        <a href="vues/v-connexion.php">Connexion</a>
+                    </div>
+            
+
         </nav>
     </header>
+
+
+<main>
 <?php
  echo '<form action="v-connexion.php" method="POST">
-
-
 
 
 <div><label for="login">Login :</label><br>
@@ -43,6 +55,8 @@ include ("../controleurs/c-connexion.php");
 
 </form>';
 ?>
+
+</main>
 
 </body>
 </html>
--- vues/v-contribution.php
+++ vues/v-contribution.php
@@ -12,19 +12,64 @@ include("../controleurs/c-contribution.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../assets/css/contribution.css">
+     <link rel="stylesheet" href="../assets/css/form.css">
+
     <title>Proposition de recette</title>
 </head>
 <body>
-    
+     
+ <header>
+        <a href="#" class="logo">LG</a>
+        <button class="burger" aria-label="Ouvrir le menu" aria-expanded="false" aria-controls="menu">
+            <img src="assets/images/burger-menu.svg" alt="">
+        </button>
 
+        <nav id="menu" aria-label="Navigation principale">
+            <ul class="navbar">
+                <li><a href="vues/recette.php">Nos Recettes</a></li>
+                <li><a href="">Aliments toxiques</a></li>
+                <li><a href="">Trouver un vétérinaire</a></li>
+                <li><a href="">Proposer une recette</a></li>
+            </ul>
+            <?php
+            if (isset($_SESSION['id_utilisateur'])) {
+                echo '<form action="controleurs/recette.php" method="get">
+                <label for="recherche" class="sr-only">Recherchez une recette</label>
+                <input type="search" name="recherche" placeholder="Recherchez une recette...">
+            </form>';
+
+                echo '<div class="compte">
+                        <a href="vues/profil.php?favoris"><img src="assets/images/favorite-on.svg" alt="Voir mes favoris"></a>
+                        <a href="vues/profil.php"><img src="assets/images/compte.svg" alt="Accéder à mon profil"></a>
+                     </div>';
+            } else {
+                echo '<div class="connexion">
+                        <a href="vues/v-inscription.php">Inscription</a>
+                        <a href="vues/v-connexion.php">Connexion</a>
+                    </div>';
+            }
+
+            ?>
+        </nav>
+    </header>
+
+<span>
+
+
+<aside>
+   
 <h1>Proposez-nous une recette !</h1>
 <p>Vous avez une recette maison que votre compagnon adore ? <br>
 Partagez-la avec nous ! 
 <br><br>
 Qu'il s'agisse de biscuits pour chiens, de friandises pour chats ou de repas faits main, nous adorons découvrir vos idées gourmandes.</p>
 
+</aside>
 
-<form action="v-contribution.php" method="POST" >
+
+
+<main>
+
 
 <div class="progress-container">
   <div class="step-progression" id="stepProgression"></div>
@@ -32,6 +77,9 @@ Qu'il s'agisse de biscuits pour chiens, de friandises pour chats ou de repas fai
   <div class="step-number">2</div>
   <div class="step-number">3</div>
 </div>
+
+<form action="v-contribution.php" method="POST" >
+
 
 <fieldset id="etape1">
 <div><label for="nom">Nom de la recette :</label>
@@ -42,6 +90,7 @@ Qu'il s'agisse de biscuits pour chiens, de friandises pour chats ou de repas fai
    <label for="categorie">Catégorie :</label>
    <select name="categorie" id="categorie">
     <option value="snack">Choisir une difficulté</option>
+    <br>
     <option value="snack">Snack</option>
     <option value="plat">Plat</option>
     <option value="dessert">Dessert</option>
@@ -50,6 +99,7 @@ Qu'il s'agisse de biscuits pour chiens, de friandises pour chats ou de repas fai
 
 <div>
    <label for="animal">Cette recette est destiné à :</label>
+   <br>
    <select name="animal" id="animal">
     <option value="">Choisir un animal</option>
     <option value="chat">Chat</option>
@@ -70,7 +120,6 @@ Qu'il s'agisse de biscuits pour chiens, de friandises pour chats ou de repas fai
 <div>
     <label for="temps">Temps de préparation (en minutes) :</label><br>
     <input type="number" id="temps" name="temps" placeholder="Ex: 15" required>
-    <span> min</span>
 </div>
 
 </fieldset>
@@ -79,7 +128,7 @@ Qu'il s'agisse de biscuits pour chiens, de friandises pour chats ou de repas fai
 <fieldset aria-describedby="ingredientTexte" id="etape2">
 <legend>Ingrédients :</legend>
 
-<button type="button" id="afficherIngredient">Ajouter des ingrédients</button>
+<button type="button" id="afficherIngredient">Sélectionner des ingrédients</button>
 <button type="button" id="masquerIngredient"> Masquer les ingrédients </button>
 
 <section id='ingredient'>
@@ -135,13 +184,17 @@ echo'<div>
 <input type="button" name="action" id="prevBtn" value="Précédent" class="button">
 
 <input type="button" name="action" id="nextBtn" value="Continuer" class="button">
+
+<input type="submit"name="action" class="button" value="Envoyer ma recette">
 </div>
 
-<input type="submit"name="action" value="Envoyer ma recette"> 
+ 
 
 </form>
 
+</main>
 
+</span>
 
 <script src="../assets/js/contribution.js"></script>
 <script src="../assets/js/inscription.js"></script>
